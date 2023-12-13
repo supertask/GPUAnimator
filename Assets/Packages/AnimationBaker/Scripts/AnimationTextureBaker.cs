349bbc75613b3426c24613e8e50ced83b4de688b
Tasuku Takahashi <takahashitasuku@team-lab.com>
2023-12-13 19:09:16 +0900

    bakeテクスチャのフォルダ構成修正

--- Assets/Packages/AnimationBaker/Scripts/AnimationTextureBaker.cs
+++ Assets/Packages/AnimationBaker/Scripts/AnimationTextureBaker.cs
@@ -42,7 +42,8 @@ namespace GPUAnimator.Baker
 
         GameObject go;
 
-        string folderPath;
+        string baseDirPath;
+        string objectDirPath;
 
         // Use this for initialization
         void Start() { }
@@ -56,37 +57,39 @@ namespace GPUAnimator.Baker
             mesh = new Mesh();
             bakedTextureAnimations = new List<BakedTextureAnimation>();
 
-            CreateBaseDirectory();
-
+            CreateBaseDirectory(objectName);
             CreateObject(objectName);
 
             foreach (AnimationState state in anim)
             {
-                StartCoroutine(Bake(state));
+                Bake(state);
             }
         }
 
-        private void CreateBaseDirectory()
+        private void CreateBaseDirectory(string objectName)
         {
-            folderPath = Path.Combine("Assets", folderName);
+            baseDirPath = Path.Combine("Assets", folderName);
 
-            if (!AssetDatabase.IsValidFolder(folderPath))
+            if (!AssetDatabase.IsValidFolder(baseDirPath))
                 AssetDatabase.CreateFolder("Assets", folderName);
+
+            objectDirPath = Path.Combine(baseDirPath, objectName);
+            if (!AssetDatabase.IsValidFolder(objectDirPath))
+            {
+                AssetDatabase.CreateFolder(baseDirPath, objectName);
+            }
         }
 
         GameObject prefab;
         void CreateObject(string objectName)
         {
-            Debug.Log("folderPath: " + folderPath);
-            if (!AssetDatabase.IsValidFolder(folderPath))
-                folderPath = AssetDatabase.CreateFolder(folderPath, objectName);
 
-            //folderPath = Path.Combine(folderPath, objectName);
+            Debug.Log("baseDirPath: " + baseDirPath);
 
             var mat = new Material(playShader);
             mat.SetTexture("_MainTex", skin.sharedMaterial.mainTexture);
 
-            AssetDatabase.CreateAsset(mat, Path.Combine(folderPath, string.Format("{0}.mat.asset", objectName)));
+            AssetDatabase.CreateAsset(mat, Path.Combine(objectDirPath, string.Format("{0}.mat.asset", objectName)));
 
             go = new GameObject(objectName);
             go.AddComponent<MeshRenderer>().sharedMaterial = mat;
@@ -96,7 +99,7 @@ namespace GPUAnimator.Baker
 
             go.AddComponent<TextureAnimations>();
 
-            var prefabPath = Path.Combine(folderPath, go.name + ".prefab").Replace("\\", "/");
+            var prefabPath = Path.Combine(objectDirPath, go.name + ".prefab").Replace("\\", "/");
 
             prefab = PrefabUtility.CreatePrefab(prefabPath, go);
 
@@ -104,11 +107,11 @@ namespace GPUAnimator.Baker
             AssetDatabase.Refresh();
         }
 
-        private IEnumerator Bake(AnimationState state)
+        private void Bake(AnimationState state)
         {
-            yield return new WaitForEndOfFrame();
+            //yield return new WaitForEndOfFrame();
 
-            //Debug.Log("name" + state.name);
+            Debug.Log("name" + state.name);
 
             anim.Play(state.name);
             frames = Mathf.NextPowerOfTwo((int)(state.length / 0.05f));
@@ -116,7 +119,7 @@ namespace GPUAnimator.Baker
             time = 0f;
             var infoList = new List<VertInfo>();
 
-            //Debug.Log("dt : " + dt);
+            Debug.Log("dt : " + dt);
 
             pRt = new RenderTexture(texWidth, frames, 0, RenderTextureFormat.ARGBHalf);
             pRt.name = string.Format("{0}.{1}.posTex", name, state.name);
@@ -130,7 +133,7 @@ namespace GPUAnimator.Baker
                 GL.Clear(true, true, Color.clear);
             }
 
-            //Debug.Log("frames : " + frames);
+            Debug.Log("frames : " + frames);
 
             for (var i = 0; i < frames; i++)
             {
@@ -173,8 +176,8 @@ namespace GPUAnimator.Baker
             bakedTextureAnimations.Add(bta);
             go.GetComponent<TextureAnimations>().SetItemSource(bakedTextureAnimations);
 
-            string posTexPath = Path.Combine(folderPath, pRt.name + ".asset");
-            string normTexPath = Path.Combine(folderPath, nRt.name + ".asset");
+            string posTexPath = Path.Combine(objectDirPath, pRt.name + ".asset");
+            string normTexPath = Path.Combine(objectDirPath, nRt.name + ".asset");
             Debug.Log("posTexPath : " + posTexPath + "normTexPath: " + normTexPath);
             AssetDatabase.CreateAsset(posTex, posTexPath);
             AssetDatabase.CreateAsset(normTex, normTexPath);
